5cd796187b29e2f15303a64799444e5a2bec3c57
Onur Dereli <onur.dereli@msuyazilim.com>
2021-05-12 02:29:32 +0300

    Photos için PhotoStock API'si hazırladı Photo için save ve delete endpointleri oluşturuldu

---
+++ Services/PhotoStock/Course.Services.PhotoStock/Controllers/PhotosController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Course.Services.PhotoStock.Dtos;
+using Course.Shared.ControllerBase;
+using Course.Shared.Dtos;
+
+namespace Course.Services.PhotoStock.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PhotosController : CustomBaseController
+    {
+        // CancellationToken; Dosya kaydetme işlemi yaparken endpoint işlemi bitince bitmesini sağlar
+        [HttpPost]
+        public async Task<IActionResult> PhotoSave(IFormFile photoFile, CancellationToken cancellationToken)
+        {
+            if (photoFile == null || photoFile.Length <= 0)
+            {
+                return CreateActionResultInstance(Response<PhotoDto>.Fail("Photo is empty", 400));
+            }   
+
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/photos", photoFile.FileName);
+
+            await using var steam = new FileStream(path, FileMode.Create);
+            await photoFile.CopyToAsync(steam, cancellationToken);
+
+            var returnPath = "photos/" + photoFile.FileName;
+
+            PhotoDto photoDto = new() { Url = returnPath};
+
+            return CreateActionResultInstance(Response<PhotoDto>.Success(photoDto, 200));
+        }
+
+        [HttpDelete]
+        public IActionResult PhotoDelete(string photoUrl)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "photos", photoUrl);
+
+            if (!System.IO.File.Exists(path))
+            {
+                return CreateActionResultInstance(Response<NoContent>.Fail("Photo not found", 404));
+            }
+
+            System.IO.File.Delete(path);
+
+            return CreateActionResultInstance(Response<NoContent>.Success(204));
+        }
+    }
+}
---
+++ Services/PhotoStock/Course.Services.PhotoStock/Startup.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Authorization;
+
+namespace Course.Services.PhotoStock
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    //kimin dağıtıcı olduğunu belirtir
+                    options.Authority = Configuration["IdentityServerUrl"];
+                    options.Audience = "photo_stock_catalog";
+                    options.RequireHttpsMetadata = false;
+                });
+            services.AddControllers(options =>
+            {
+                options.Filters.Add(new AuthorizeFilter());
+            });
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Course.Services.PhotoStock", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Course.Services.PhotoStock v1"));
+            }
+
+            // Dosyalara dışarıdan public olarak eşirim sağlar
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
