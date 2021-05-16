3ffe6dbc1391695f5006703e1f74afa9f044fa05
Onur Dereli <onur.dereli@msuyazilim.com>
2021-05-16 21:27:23 +0300

    Response static methodlarda düzenleme yapıldı

--- Shared/Course.Shared/Dtos/Response.cs
+++ Shared/Course.Shared/Dtos/Response.cs
@@ -16,24 +16,12 @@ namespace Course.Shared.Dtos
         public List<string> Errors { get; private set; }
 
         //Static Factory Methods
-        public static Response<T> Success(T data, int statusCode)
-        {
-            return new Response<T> { Data = data, StatusCode = statusCode, IsSuccessfull = true };
-        }
+        public static Response<T> Success(T data, int statusCode) => new Response<T> { Data = data, StatusCode = statusCode, IsSuccessfull = true };
 
-        public static Response<T> Success(int statusCode)
-        {
-            return new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessfull = true };
-        }
+        public static Response<T> Success(int statusCode) => new Response<T> { Data = default(T), StatusCode = statusCode, IsSuccessfull = true };
 
-        public static Response<T> Fail(List<string> errors, int statusCode)
-        {
-            return new Response<T> { Errors = errors, StatusCode = statusCode, IsSuccessfull = false };
-        }
+        public static Response<T> Fail(List<string> errors, int statusCode) => new Response<T> { Errors = errors, StatusCode = statusCode, IsSuccessfull = false, Data = default };
 
-        public static Response<T> Fail(string error, int statusCode)
-        {
-            return new Response<T> { Errors = new List<string> { error }, StatusCode = statusCode, IsSuccessfull = false };
-        }
+        public static Response<T> Fail(string error, int statusCode) => new Response<T> { Errors = new List<string> { error }, StatusCode = statusCode, IsSuccessfull = false };
     }
 }
