0b0be0c9fe97a947b82a8f3cae6b0a543bd84450
Onur Dereli <info@onurdereli.com>
2021-08-17 15:33:59 +0300

    UnAuthorizeException hatası için kontrol eklendi

--- Frondends/Course.Web/Controllers/HomeController.cs
+++ Frondends/Course.Web/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Course.Web.Exceptions;
+using Course.Web.Services.Abstract;
+using Microsoft.AspNetCore.Diagnostics;
 
 namespace Course.Web.Controllers
 {
@@ -13,14 +16,22 @@ namespace Course.Web.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ICatalogService _catalogService;
+
+        public HomeController(ILogger<HomeController> logger, ICatalogService catalogService)
         {
             _logger = logger;
+            _catalogService = catalogService;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
+            return View(await _catalogService.GetAllCourse());
+        }
+
+        public async Task<IActionResult> Detail(string id)
+        {
+            return View(await _catalogService.GetByCourseId(id));
         }
 
         public IActionResult Privacy()
@@ -31,6 +42,13 @@ namespace Course.Web.Controllers
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
+            // UnAuthorizeException hatası geldiğinde kullanıcıya logout yaptırmayı sağlar. Bunun amacı accesstoken süresi bittiğinde error almamak için.
+            var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>();
+            if(exceptionHandlerFeature is { Error: UnAuthorizeException })
+            {
+                return RedirectToAction(nameof(AuthController.Logout), "Auth");
+            }
+
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
